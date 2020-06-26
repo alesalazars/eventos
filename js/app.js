b65ac74379cb02c960ca5d6beb392439f0e8188c
Ale <ma.salazarsanchez@gmail.com>
2020-06-25 22:34:31 -0400

    Validating form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 // Instanciar ambas clases
 const eventbrite = new Eventbrite();
 const ui = new Interfaz();
+
+// Listener al buscador
+document.getElementById('buscarBtn').addEventListener('click', (e) => {
+  e.preventDefault();
+  
+  // Leer el texto del input buscar
+  const textoBuscador = document.getElementById('evento').value;
+
+  // leer el select
+  const categorias = document.getElementById('listado-categorias');
+  const categoriaSeleccionada = categorias.options[categorias.selectedIndex].value;
+
+  console.log(textoBuscador)
+  console.log(categoriaSeleccionada)
+
+  // revisar que haya algo escrito en el buscador
+  if(textoBuscador !== ''){
+    console.log('Buscando...')
+  }else{
+    // mostrar mensaje para que imprima algo
+    ui.mostrarMensaje('Escribe algo en el buscador', 'alert alert-danger mt-4');
+  }
+})
